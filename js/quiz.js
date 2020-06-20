a3a9fe4ecd287539cc097d005f4a5d13f68ce7c7
Andreza Alves <drezlves@gmail.com>
2020-06-20 00:13:33 -0300

    att

--- js/quiz.js
+++ js/quiz.js
@@ -2,31 +2,32 @@
 function check(){
 
 	var question1 = document.quiz.question1.value;
-	var question2 = document.quiz.question2.value;
-	var question3 = document.quiz.question3.value;
-	var question4 = document.quiz.question4.value;
-	var question5 = document.quiz.question5.value;
-	var question6 = document.quiz.question6.value;
+	var question2 = document.quiz.question2;
+	var question3 = document.quiz.question3;
+	var question4 = document.quiz.question4;
+	var question5 = document.quiz.question5;
+	var question6 = document.quiz.question6;
 	var correct = 0;
 
 
 	if (question1 == "Halloween") {
 		correct++;
 }
-	if (question2 == "Duro de Matar") {
+	if (question2[0].checked) {
 		correct++;
 }	
-	if (question3 == "Cheddar") {
+
+	if (question3[1].checked) {
 		correct++;
 	}
 
-	if (question4 == "Doug Judy") {
+	if (question4[0].checked) {
 		correct++;
 }
-	if (question5 == "Taylor Swift") {
+	if (question5[0].checked) {
 		correct++;
 }	
-	if (question6 == "Jake") {
+	if (question6[1].checked) {
 		correct++;
 	}
 	
